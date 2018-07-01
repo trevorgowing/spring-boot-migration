0d9521a559b359ccb0d2a8c07cd0b0461b5445e9
Trevor Gowing <trev.gowing@gmail.com>
2018-07-01 14:26:13 +0200

    Domain

--- src/main/java/com/trevorgowing/springbootmigration/patient/Patient.java
+++ src/main/java/com/trevorgowing/springbootmigration/patient/Patient.java
@@ -11,21 +11,12 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.index.CompoundIndex;
-import org.springframework.data.mongodb.core.index.CompoundIndexes;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Getter
 @Setter
 @ToString
 @Document(collection = "patients")
-@CompoundIndexes({
-  @CompoundIndex(
-    name = "name_system_value_uidx",
-    unique = true,
-    def = "{'identifiers.system' : 1, 'identifiers.name': 1}"
-  )
-})
 class Patient implements Serializable {
 
   private static final long serialVersionUID = 4201950435560110314L;
